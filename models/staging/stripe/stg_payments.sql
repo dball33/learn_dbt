d11d55633b7d0c826b5d301ad5088dcba18a49f5
dng@qw-corp.com <dng@qw-corp.com>
2021-06-02 13:29:01 +0000

    20210602 01

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,12 +1,12 @@
 with payments as (
 
     select
-        orderid as order_id
+        id as payment_id
+        ,orderid as order_id
         ,paymentmethod as payment_method
         ,amount/100 as amount
         ,status
         ,created as created_at
-        ,id as payment_id
     from {{ source('stripe','payment' )}}
 )
 
